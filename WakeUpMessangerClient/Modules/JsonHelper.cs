6fb10d27c4820f01b36c86a08d353ffa10bbb9d8
Serenity <kim910320@naver.com>
2015-10-05 23:44:30 +0900

    - Json 관련 클래스 추가

---
+++ WakeUpMessangerClient/Modules/JsonHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Json;
+
+namespace WakeUpMessangerClient.Modules
+{
+    public class JsonName
+    {
+        public const string ID = "ID";
+        public const string Password = "Password";
+    }
+
+    public class JsonHelper
+    {
+        public JsonHelper()
+        {
+
+        }
+
+        public string GetLoginInfo(string id, string password)
+        {
+            JsonObjectCollection jsonObjectCollection = new JsonObjectCollection();
+
+            jsonObjectCollection.Add(new JsonStringValue(JsonName.ID, id));
+            jsonObjectCollection.Add(new JsonStringValue(JsonName.Password, password));
+
+            return jsonObjectCollection.ToString();
+        }
+    }
+}
