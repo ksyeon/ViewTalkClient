37d8f2b0430110dd40f5f64342d987bb563a77c9
Serenity <kim910320@naver.com>
2015-10-07 02:13:23 +0900

    - JoinView 추가 - ChattingView 디자인 변경 - Resource Dictionary 추가 (Style 적용)

---
+++ WakeUpMessangerClient/ViewModels/JoinViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WakeUpMessangerClient.ViewModels
+{
+    public class JoinViewModel
+    {
+        public JoinViewModel()
+        {
+
+        }
+    }
+}
